24ebd767525b6986508cdbcc01fbb7019812af35
vladimir <sokoltsovvs@gmail.com>
2023-07-30 23:54:18 +0600

    add x5.ru example tests

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -1,18 +1,9 @@
 package tests;
 
-import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-
-import java.util.List;
-import java.util.stream.Stream;
-
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
